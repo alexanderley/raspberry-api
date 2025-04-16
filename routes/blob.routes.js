89048ede0f7005bc0cc57b012e7d8bdd930184e9
alexanderley <alexander.ley.inbox@gmail.com>
2025-04-16 19:09:24 +0100

    fixed typo in blob routes

--- routes/blob.routes.js
+++ routes/blob.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // upload video to blob
 router.post("/upload", (req, res) => {
-
+    
 });
 
 
@@ -12,3 +12,5 @@ router.get("/preview/:videoId", (req, res) => {});
 
 // get video file
 router.get("/video/:videoId", (req, res) => {});
+
+module.exports = router;
