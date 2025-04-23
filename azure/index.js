2f760c3df1b8e250f2a45918b4a766bae79a2b1c
alexanderley <112874444+alexanderley@users.noreply.github.com>
2025-04-23 20:07:49 +0200

    added blob connection

--- azure/index.js
+++ azure/index.js
@@ -1,40 +1,11 @@
-import {BlobServiceClient} from '@azure/storage-blob';
-
+// impo {BlobServiceClient} from '@azure/storage-blob';
+const test = process.env.TEST
 const accountName = process.env.AZURE_ACCOUNT_NAMEE;
 const sasToken = process.env.SAS_TOKEN;
 const containerName = process.env.AZURE_CONTAINER_NAME;
 
-const blobServiceClient = new BlobServiceClient(
-  `https://${accountName}.blob.core.windows.net${sasToken}`
-);
-
-const containerClient = blobServiceClient.getContainerClient(containerName);
-
-
-
-export async function testConnection() {
-    
+console.log("test", test);
 // console.log("accountName", accountName);
 // console.log("sasToken", sasToken);
 // console.log("containerName", containerName);
-// console.log("blobServiceClient", blobServiceClient);
-    console.log("Testing Azure Blob Storage connection...");
-    try {
-      console.log("Testing Azure Blob Storage connection...");
-      
-      // List blobs (even if empty, this confirms the container is accessible)
-      let i = 1;
-      for await (const blob of containerClient.listBlobsFlat()) {
-        console.log(`Blob ${i++}: ${blob.name}`);
-      }
-  
-      console.log("✅ Connection successful! Container is accessible.");
-      return true;
-    } catch (error) {
-      console.error("❌ Connection failed:", error.message);
-      return false;
-    }
-  }
-
-export {blobServiceClient,containerClient};
 
--- server.js
+++ server.js
@@ -1,17 +1,40 @@
+
+require('dotenv').config();
+const { ContainerClient, BlobServiceClient } = require("@azure/storage-blob");
+
+
+const accountName = process.env.AZURE_ACCOUNT_NAME;
+const sasToken = process.env.SAS_TOKEN;
+const containerName = process.env.AZURE_ACCOUNT_NAME;
+
+// Establishes a connection with Azure Blob Storage
+const blobServiceClient = new BlobServiceClient(`https://${accountName}.blob.core.windows.net/?${sasToken}`);
+// const containerClient = blobServiceClient.getContainerClient(containerName);
+
+async function testConnection(){
+  try{
+    const props = await blobServiceClient.getProperties();
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    console.log('✓ Connection successful!');
+    console.log(`Account SKU: ${props}`);
+    console.log('XXXXXX containerclient: ', containerClient);
+    return true;
+  }catch(err){
+    console.log('error: ',err.message)
+  }
+}
+testConnection();
+
 const app = require("./app");
-// const cors = require('cors');
-
-const {testConnection} = require("./azure/index.js");
-
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 5005;
-
 
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
 
 
-console.log("AZURE_CONTAINER_NAME", process.env.AZURE_CONTAINER_NAME);
-console.log("SAS_TOKEN", process.env.SAS_TOKEN);
-console.log("AZURE_ACCOUNT_NAME", process.env.AZURE_ACCOUNT_NAME);
+const express = require('express');
+
+
+
