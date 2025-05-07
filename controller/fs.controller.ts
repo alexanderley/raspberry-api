460a66df19fefe7413aba49cd8bdcb5bceb8ff00
alexanderley <112874444+alexanderley@users.noreply.github.com>
2025-05-07 16:19:32 +0200

    added typescript

---
+++ controller/fs.controller.ts
@@ -0,0 +1,28 @@
+// This file contains filesystem logic in order to read and write files to the server
+const router = Router();
+const TEMP_DIR = path.join(__dirname, 'temp');
+const EXPORT_DIR = path.join(__dirname, 'export');
+
+// Ensures directory exists
+function ensureDirExists(dirPath: string) {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath, { recursive: true });
+  }
+}
+
+// Saves incoming stream to disk
+async function saveToTempFile(req, filePath: string): Promise<void> {
+  ensureDirExists(path.dirname(filePath));
+  const writeStream = fs.createWriteStream(filePath);
+  return new Promise((resolve, reject) => {
+    req.pipe(writeStream)
+      .on('finish', resolve)
+      .on('error', reject);
+  });
+}
+
+
+module.exports = {
+    ensureDirExists,
+    saveToTempFile
+}
