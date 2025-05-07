de25762e1592eb9625fbcdf12ce5951298e90524
alexanderley <112874444+alexanderley@users.noreply.github.com>
2025-05-07 20:53:45 +0200

    push

--- controller/fs.controller.js
+++ controller/fs.controller.js
@@ -1,28 +1,34 @@
-// This file contains filesystem logic in order to read and write files to the server
-const router = Router();
-const TEMP_DIR = path.join(__dirname, 'temp');
-const EXPORT_DIR = path.join(__dirname, 'export');
+// // This file contains filesystem logic in order to read and write files to the server
+// // # TODO make shure that this logic is working?
 
-// Ensures directory exists
-function ensureDirExists(dirPath: string) {
-  if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath, { recursive: true });
-  }
-}
+// import fs from 'fs';
+// import path from 'path';
+// import { Router } from 'express';
+// import { IncomingMessage } from 'http';
 
-// Saves incoming stream to disk
-async function saveToTempFile(req, filePath: string): Promise<void> {
-  ensureDirExists(path.dirname(filePath));
-  const writeStream = fs.createWriteStream(filePath);
-  return new Promise((resolve, reject) => {
-    req.pipe(writeStream)
-      .on('finish', resolve)
-      .on('error', reject);
-  });
-}
+// const TEMP_DIR = path.join(__dirname, 'temp');
+// const EXPORT_DIR = path.join(__dirname, 'export');
+
+// // Ensures directory exists
+// function ensureDirExists(dirPath: string) {
+//   if (!fs.existsSync(dirPath)) {
+//     fs.mkdirSync(dirPath, { recursive: true });
+//   }
+// }
+
+// // Saves incoming stream to disk
+// async function saveToTempFile(req: IncomingMessage, filePath: string): Promise<void> {
+//   ensureDirExists(path.dirname(filePath));
+//   const writeStream = fs.createWriteStream(filePath);
+//   return new Promise((resolve, reject) => {
+//     req.pipe(writeStream)
+//       .on('finish', resolve)
+//       .on('error', reject);
+//   });
+// }
 
 
-module.exports = {
-    ensureDirExists,
-    saveToTempFile
-}
+// module.exports = {
+//     ensureDirExists,
+//     saveToTempFile
+// }
