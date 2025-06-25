736a1493d94291676393a3a47a7d9eb7d7661a5f
alexanderley <112874444+alexanderley@users.noreply.github.com>
2025-06-25 18:14:57 +0200

    cleaned up blob code

--- routes/video.routes.js
+++ routes/video.routes.js
@@ -2,11 +2,6 @@ const express = require("express");
 const router = express.Router();
 const fs = require('fs');
 
-// const videoFileMap = {
-//   'cdn': "videos/cdn.mp4",
-//   'generate-pass' : 'temp/waterfall.mp4',
-//   'get-post': 'temp/waterfall.mp4',
-// }
 const videoFileMap = {
   'waterfall': "routes/temp/waterfall.mp4",
   'parfume' : 'routes/temp/parfume.mp4',
@@ -22,13 +17,8 @@ router.get('/streamVideo/:filename', async (req,res) => {
       return res.status(404).send('File not found')
     }
 
-    if(filePath){
-      console.log('filePath exist', filePath)
-    }
-
     const stat = fs.statSync(filePath);
     const fileSize = stat.size;
-    // const range = req.header.range;
     const range = req.headers.range;
 
     if(stat){
@@ -43,11 +33,6 @@ router.get('/streamVideo/:filename', async (req,res) => {
       const chunksize = end - start + 1;
       const file = fs.createReadStream(filePath, {start, end});
 
-      // const file = fs.createReadStream(filePath, { start, end });
-      // file.on('error', (err) => {
-      //   console.error('Stream error:', err);
-      //   res.status(500).end();
-      // });
 
       const head = {
         'Content-Range': `bytes ${start}-${end}/${fileSize}`,
